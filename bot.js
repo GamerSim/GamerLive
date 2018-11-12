40ec2e36e166528d67f48643c70a20e6a5ebcdd2
GamerSim <44953626+GamerSim@users.noreply.github.com>
2018-11-12 18:53:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Don't Give Up`,"http://twitch.tv/Death")
+client.user.setGame(`Don't Give Up`,"https://www.twitch.tv/iGamerSim")
 client.user.setStatus("dnd")
 });
 
